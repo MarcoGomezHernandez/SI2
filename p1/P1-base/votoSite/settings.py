b0fe63f515c32a3fabc5ce7488396c36cd0e98f3
Marco Gomez Hernandez <marco.gomezh@estudiante.uam.es>
2025-02-10 19:36:37 +0100

    sdsddad

--- p1/P1-base/votoSite/settings.py
+++ p1/P1-base/votoSite/settings.py
@@ -26,8 +26,8 @@ load_dotenv(dotenv_path=env_file)
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-alczftn)j1#$v%xmk@5j(n*px43c8kxgi_ua4%khc+t7g_)s9d'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-alczftn)j1#$v%xmk@5j(n*px43c8kxgi_ua4%khc+t7g_)s9d'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
